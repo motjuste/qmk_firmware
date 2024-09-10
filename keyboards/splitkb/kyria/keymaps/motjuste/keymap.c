1f418d22e1ae9fd9b840986ca6513da3fe4652bc
motjuste <abdullah.motjuste@gmail.com>
2024-09-10 03:20:58 +0200

    actually make me toggle it off with the other hand

--- keyboards/splitkb/kyria/keymaps/motjuste/keymap.c
+++ keyboards/splitkb/kyria/keymaps/motjuste/keymap.c
@@ -56,14 +56,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB , KC_Q   , KC_W   , KC_D   , KC_F   , KC_K   ,                                         KC_J   , KC_U   , KC_R   , KC_L   , KC_SCLN, KC_LBRC,
         KC_LCTL, KC_A   , KC_S   , KC_E   , KC_T   , KC_G   ,                                         KC_Y   , KC_N   , KC_I   , KC_O   , KC_H   , KC_QUOT,
         KC_LSFT, KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   , XXXXXXX, XXXXXXX,     XXXXXXX, XXXXXXX, KC_P   , KC_M   , KC_COMM, KC_DOT , KC_SLSH, KC_RSFT,
-                                   XXXXXXX, KC_LALT, KC_LCMD, KC_SPC , TNUMNAV,     TNUMNAV, KC_BSPC, KC_RCMD, KC_ALGR, XXXXXXX
+                                   XXXXXXX, KC_LALT, KC_LCMD, KC_SPC , XXXXXXX,     TNUMNAV, KC_BSPC, KC_RCMD, KC_ALGR, XXXXXXX
     ),
 
     [_NUMNAV] = LAYOUT(
         KC_GRV , KC_1   , KC_2   , KC_3   , KC_4   , KC_5   ,                                         KC_VOLU, KC_MPRV, KC_UP  , KC_MNXT, _______, _______,
         _______, KC_6   , KC_7   , KC_8   , KC_9   , KC_0   ,                                         KC_VOLD, KC_LEFT, KC_DOWN, KC_RGHT, KC_DEL , _______,
         _______, KC_LBRC, KC_RBRC, KC_BSLS, KC_MINS, KC_EQL , XXXXXXX, XXXXXXX,     XXXXXXX, XXXXXXX, KC_MPLY, KC_MUTE, _______, _______, _______, _______,
-                                   XXXXXXX, _______, _______, _______, _______,     _______, _______, _______, _______, XXXXXXX
+                                   XXXXXXX, _______, _______, _______, TNUMNAV,     XXXXXXX, _______, _______, _______, XXXXXXX
     ),
 
     // TODO, without any modifiers being triggered:
