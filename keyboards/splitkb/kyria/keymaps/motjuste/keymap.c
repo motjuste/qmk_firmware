ab8de9b3d822c275b1360d1272fc21b6c8c1faf1
motjuste <abdullah.motjuste@gmail.com>
2024-09-12 03:39:21 +0200

    fix symbol layer not picking up nav keys from number layer; that was wrong

--- keyboards/splitkb/kyria/keymaps/motjuste/keymap.c
+++ keyboards/splitkb/kyria/keymaps/motjuste/keymap.c
@@ -63,16 +63,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
 
     [_SYMBOL] = LAYOUT(
-        KC_TILD, KC_EXLM, KC_AT  , KC_HASH, KC_DLR , KC_PERC,                                         _______, _______, _______, _______, KC_COLN, KC_LCBR,
-        _______, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN,                                         _______, _______, _______, _______, _______, KC_DQUO,
-        _______, KC_LCBR, KC_RCBR, KC_UNDS, KC_PLUS, KC_PIPE, _______, KC_RCBR,     KC_PIPE, _______, _______, _______, KC_LABK, KC_RABK, KC_QUES, _______,
+        KC_TILD, KC_EXLM, KC_AT  , KC_HASH, KC_DLR , KC_PERC,                                         KC_VOLU, KC_MPRV, KC_UP  , KC_MNXT, KC_COLN, KC_LCBR,
+        _______, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN,                                         KC_VOLD, KC_LEFT, KC_DOWN, KC_RGHT, KC_DEL , KC_DQUO,
+        _______, KC_LCBR, KC_RCBR, KC_UNDS, KC_PLUS, KC_PIPE, _______, KC_RCBR,     KC_PIPE, _______, KC_MPLY, KC_MUTE, KC_LABK, KC_RABK, KC_QUES, _______,
                                    _______, _______, _______, _______, _______,     _______, _______, _______, _______, _______
     ),
 
     [_FUNNAV] = LAYOUT(
-        KC_TILD, KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5  ,                                         _______, _______, _______, _______, _______, _______,
-        _______, KC_F6  , KC_F7  , KC_F8  , KC_F9  , KC_F10 ,                                         _______, _______, _______, _______, _______, _______,
-        _______, KC_BRID, KC_BRIU, KC_F11 , KC_F12 , KC_CAPS, NORMAN , QWERTY ,     _______, _______, _______, _______, _______, _______, _______, _______,
+        KC_TILD, KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5  ,                                         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+        _______, KC_F6  , KC_F7  , KC_F8  , KC_F9  , KC_F10 ,                                         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______,
+        _______, KC_BRID, KC_BRIU, KC_F11 , KC_F12 , KC_CAPS, NORMAN , QWERTY ,     _______, _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______,
                                    _______, _______, _______, _______, _______,     _______, _______, _______, _______, _______
         //
         // IDEA: make right side mouse keys
@@ -87,16 +87,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 oled_rotation_t oled_init_user(oled_rotation_t rotation) { return OLED_ROTATION_180; }
 
 bool oled_task_user(void) {
+    oled_write_P(PSTR("\n\n\n"), false);
     if (is_keyboard_master()) {
-        // QMK Logo and version information
-        // clang-format off
-        static const char PROGMEM qmk_logo[] = {
-            0x80,0x81,0x82,0x83,0x84,0x85,0x86,0x87,0x88,0x89,0x8a,0x8b,0x8c,0x8d,0x8e,0x8f,0x90,0x91,0x92,0x93,0x94,
-            0xa0,0xa1,0xa2,0xa3,0xa4,0xa5,0xa6,0xa7,0xa8,0xa9,0xaa,0xab,0xac,0xad,0xae,0xaf,0xb0,0xb1,0xb2,0xb3,0xb4,
-            0xc0,0xc1,0xc2,0xc3,0xc4,0xc5,0xc6,0xc7,0xc8,0xc9,0xca,0xcb,0xcc,0xcd,0xce,0xcf,0xd0,0xd1,0xd2,0xd3,0xd4,0};
-        // clang-format on
+        // // QMK Logo and version information
+        // // clang-format off
+        // static const char PROGMEM qmk_logo[] = {
+        //     0x80,0x81,0x82,0x83,0x84,0x85,0x86,0x87,0x88,0x89,0x8a,0x8b,0x8c,0x8d,0x8e,0x8f,0x90,0x91,0x92,0x93,0x94,
+        //     0xa0,0xa1,0xa2,0xa3,0xa4,0xa5,0xa6,0xa7,0xa8,0xa9,0xaa,0xab,0xac,0xad,0xae,0xaf,0xb0,0xb1,0xb2,0xb3,0xb4,
+        //     0xc0,0xc1,0xc2,0xc3,0xc4,0xc5,0xc6,0xc7,0xc8,0xc9,0xca,0xcb,0xcc,0xcd,0xce,0xcf,0xd0,0xd1,0xd2,0xd3,0xd4,0};
+        // // clang-format on
 
-        oled_write_P(qmk_logo, false);
+        // oled_write_P(qmk_logo, false);
         oled_write_P(PSTR("motjuste.dev\n\n"), false);
 
         // Host Keyboard Layer Status
