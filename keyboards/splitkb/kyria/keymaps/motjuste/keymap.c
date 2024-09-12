bbce630ad0926dca4bf8255423066dc5d6b786e7
motjuste <abdullah.motjuste@gmail.com>
2024-09-12 20:33:44 +0200

    I give up also on pure left shift ... space cadet all the way

--- keyboards/splitkb/kyria/keymaps/motjuste/keymap.c
+++ keyboards/splitkb/kyria/keymaps/motjuste/keymap.c
@@ -43,14 +43,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_NORMAN] = LAYOUT(
         KC_TAB , KC_Q   , KC_W   , KC_D   , KC_F   , KC_K   ,                                         KC_J   , KC_U   , KC_R   , KC_L   , KC_SCLN, KC_MINS,
         CTL_ESC, KC_A   , KC_S   , KC_E   , KC_T   , KC_G   ,                                         KC_Y   , KC_N   , KC_I   , KC_O   , KC_H   , CTL_QUO,
-        KC_LSFT, KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   , KC_ENT , KC_RBRC,     KC_BSLS, KC_ESC , KC_P   , KC_M   , KC_COMM, KC_DOT , KC_SLSH, SC_SENT,
+        SC_LSPO, KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   , KC_ENT , KC_RBRC,     KC_BSLS, KC_ESC , KC_P   , KC_M   , KC_COMM, KC_DOT , KC_SLSH, SC_SENT,
                                    LFUNBSP, KC_LALT, KC_SPC , KC_LCMD, TNUMNAV,     TSYMNAV, CMD_LBR, KC_BSPC, ALT_EQL, LFUNSPC
     ),
 
     [_QWERTY] = LAYOUT(
         KC_TAB , KC_Q   , KC_W   , KC_E   , KC_R   , KC_T   ,                                         KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   , KC_MINS,
         CTL_ESC, KC_A   , KC_S   , KC_D   , KC_F   , KC_G   ,                                         KC_H   , KC_J   , KC_K   , KC_L   , KC_SCLN, CTL_QUO,
-        KC_LSFT, KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   , KC_ENT , KC_RBRC,     KC_BSLS, KC_ESC , KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLSH, SC_SENT,
+        SC_LSPO, KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   , KC_ENT , KC_RBRC,     KC_BSLS, KC_ESC , KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLSH, SC_SENT,
                                    LFUNBSP, KC_LALT, KC_SPC , KC_LCMD, TNUMNAV,     TSYMNAV, CMD_LBR, KC_BSPC, ALT_EQL, LFUNSPC
     ),
 
