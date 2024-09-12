a6ac3e1e2a2422c75b469202bea237cb74dcb7d2
motjuste <abdullah.motjuste@gmail.com>
2024-09-12 13:52:27 +0200

    ongoing experiments with oled

--- keyboards/splitkb/kyria/keymaps/motjuste/keymap.c
+++ keyboards/splitkb/kyria/keymaps/motjuste/keymap.c
@@ -87,40 +87,29 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 oled_rotation_t oled_init_user(oled_rotation_t rotation) { return OLED_ROTATION_180; }
 
 bool oled_task_user(void) {
-    oled_write_P(PSTR("\n\n\n"), false);
+    const uint8_t current_layer = get_highest_layer(layer_state|default_layer_state);
     if (is_keyboard_master()) {
-        // // QMK Logo and version information
-        // // clang-format off
-        // static const char PROGMEM qmk_logo[] = {
-        //     0x80,0x81,0x82,0x83,0x84,0x85,0x86,0x87,0x88,0x89,0x8a,0x8b,0x8c,0x8d,0x8e,0x8f,0x90,0x91,0x92,0x93,0x94,
-        //     0xa0,0xa1,0xa2,0xa3,0xa4,0xa5,0xa6,0xa7,0xa8,0xa9,0xaa,0xab,0xac,0xad,0xae,0xaf,0xb0,0xb1,0xb2,0xb3,0xb4,
-        //     0xc0,0xc1,0xc2,0xc3,0xc4,0xc5,0xc6,0xc7,0xc8,0xc9,0xca,0xcb,0xcc,0xcd,0xce,0xcf,0xd0,0xd1,0xd2,0xd3,0xd4,0};
-        // // clang-format on
-
-        // oled_write_P(qmk_logo, false);
-        oled_write_P(PSTR("motjuste.dev\n\n"), false);
-
-        // Host Keyboard Layer Status
-        oled_write_P(PSTR("Layer: "), false);
-        switch (get_highest_layer(layer_state|default_layer_state)) {
+        oled_set_cursor(0, 3);
+        switch (current_layer) {
             case _NORMAN:
-                oled_write_P(PSTR("NORMAN\n"), false);
+                oled_write_P(PSTR("norman"), false);
                 break;
             case _QWERTY:
-                oled_write_P(PSTR("QWERTY\n"), false);
+                oled_write_P(PSTR("QWERTY"), false);
                 break;
-            case _NUMBER    :
-                oled_write_P(PSTR("NUMBER\n"), false);
+            case _NUMBER:
+                oled_write_P(PSTR("NUMBER"), false);
                 break;
             case _SYMBOL:
-                oled_write_P(PSTR("SYMBOL\n"), false);
+                oled_write_P(PSTR("SYMBOL"), false);
                 break;
             case _FUNNAV:
-                oled_write_P(PSTR("FUNNAV\n"), false);
+                oled_write_P(PSTR("FUNNAV"), false);
                 break;
             default:
-                oled_write_P(PSTR("Undefined\n"), false);
+                oled_write_P(PSTR("Undefined"), false);
         }
+        oled_write_P(PSTR("       \n"), true);
 
         // Write host Keyboard LED Status to OLEDs
         led_t led_usb_state = host_keyboard_led_state();
