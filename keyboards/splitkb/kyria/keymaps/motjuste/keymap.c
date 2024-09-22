399c089b551874f286528ae208000031dde3f7cc
motjuste <abdullah.motjuste@gmail.com>
2024-09-22 15:34:51 +0200

    update copyright information

--- keyboards/splitkb/kyria/keymaps/motjuste/keymap.c
+++ keyboards/splitkb/kyria/keymaps/motjuste/keymap.c
@@ -1,4 +1,4 @@
-/* Copyright 2019 Thomas Baart <thomas@splitkb.com>
+/* Copyright 2024 Abdullah (@motjuste) <abdullah.motjuste@gmail.com>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
